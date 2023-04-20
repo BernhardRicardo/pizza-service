288609c5879d58dcb72ee3035a5d06edc4ffae32
stvitanh <vinsky.s.tanhansen@stud.h-da.de>
2023-04-20 17:10:34 +0200

    fix button id

--- src/Praktikum/Prak1/baecker.php
+++ src/Praktikum/Prak1/baecker.php
@@ -86,8 +86,8 @@ class Baecker extends Page
         <h1>Baecker</h1>
         <h2>Bestellung No.17</h2>
         <input type="radio" id="zubereitung" name="pizza" value="Salami">Zubereiten<br>
-        <input type="radio" id="zubereitung" name="pizza" value="Salami">im Ofen<br>
-        <input type="radio" id="zubereitung" name="pizza" value="Salami">abholbereits<br>
+        <input type="radio" id="imOfen" name="pizza" value="Salami">im Ofen<br>
+        <input type="radio" id="abholbereits" name="pizza" value="Salami">abholbereits<br>
         HTML;
         // to do: output view of this page
         $this->generatePageFooter();
